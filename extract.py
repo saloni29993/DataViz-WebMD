5f5db3170ec1502478d6c991340c25b555b4d85b
Kashyap Bhansali <kashyapbhansali7@gmail.com>
2016-10-01 21:18:27 -0700

    txt to list for drugs and symtoms. need to fix medicaltests scraper

--- extract.py
+++ extract.py
@@ -4,22 +4,30 @@ from pprint import pprint
 
 data_answers = []
 data_questions = []
+drugs = []
+symptoms = []
+medicaltests = []
 
-with open('webmd-answer.json') as data_file:    
-    data_answers = json.load(data_file)
+# with open('dataset/webmd-answer.json') as data_file:    
+#     data_answers = json.load(data_file)
 
-with open('webmd-question.json') as data_file:
-	data_questions = json.load(data_file)
+# with open('dataset/webmd-question.json') as data_file:
+# 	data_questions = json.load(data_file)
+
+#loading drugs
+drugs = [line.strip('\n\t\r') for line in open('ScrapedData/drugs.txt')]
+
+#loading symptoms
+symptoms = [line.strip('\n\t\r') for line in open('ScrapedData/symptoms.txt')]
+
+#loading medicaltests
+medicaltests = [line.strip('\n\t\r') for line in open('ScrapedData/medicaltests.txt')]
 
 
-#iterating through questions
-for q in data_questions:
-	#getting answers based on question id
-	
-	answer_blocks = filter(lambda x: x['questionId'] == q['questionId'], data_answers)
-	pprint(answer_blocks)
-	# count = len(answer_blocks);
-	# if count != 1:
-	# 	print "\n**** New Question *****"
-	# 	print count 
+# #iterating through questions
+# for q in data_questions:
+# 	#getting answers based on question id
+# 	answer_blocks = filter(lambda x: x['questionId'] == q['questionId'], data_answers)
+# 	pprint(answer_blocks)
 
+print drugs
